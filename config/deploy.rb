150f88c3974e221dd35ec2c70bd85708774cfa99
Ken Burgett <keburgett@gmail.com>
2009-06-05 15:26:08 -0700

    added capistrano and passenger support

---
+++ config/deploy.rb
@@ -0,0 +1,69 @@
+#
+# Application-specific settings
+#
+set :rails_env,           "production"
+set :application,         'fulfill'
+#
+#Server specific settings
+#
+set :domain,              'vscdev.com'
+set :user,                "vscdevco"
+set :password,            "dr8k3sb8d"
+set :use_sudo,            false
+set :site5_symlink,	      "/home/#{user}/public_html/#{application}"
+#
+# SCM and repository settings
+#
+set :scm,                 'git'
+set :repository,          "git@github.com:explainer/fulfill.git "
+set :branch,              'master'
+set :scm_verbose,         true
+#
+# deployment settings
+#
+set :deploy_to,           "/home/#{user}/#{rails_env}/#{application}"
+set :deploy_via,          :remote_cache
+set :keep_releases,       5
+#
+# roles
+#
+role :app,                domain
+role :db,                 domain, :primary => true
+role :web,                domain
+
+namespace :deploy do
+  task :symlink, :except => { :no_release => true } do
+    #
+    # In reality, there are two symlink needed for a complete deploy.
+    # The first one createc a 'current' link in the production/application folder to
+    # the latest folder in the releases folder
+    run "rm -f #{current_path} && ln -s #{latest_release} #{current_path}"
+    #
+    # The second symlink creates a link in the public_html folder which points to the public
+    # folder in 'current'
+    run "rm -f #{site5_symlink} && ln -s #{current_path}/public #{site5_symlink}"
+  end
+
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  #
+  # task which causes Passenger to initiate a restart
+  #
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,9 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
+# This magic line is necessary to allow multiple rails apps to share a host server.
+config.action_controller.relative_url_root = "/fulfill"
+
 # See everything in the log (default is :info)
 # config.log_level = :debug
 
