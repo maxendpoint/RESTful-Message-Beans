8bc1458d36b43540f1159939733b4c14073ca58b
Ken Burgett <keburgett@gmail.com>
2009-06-15 11:58:49 -0700

    minor mods to views

--- app/models/mechanize_upload_test.rb
+++ app/models/mechanize_upload_test.rb
@@ -7,4 +7,15 @@ require 'mechanize'
 agent = WWW::Mechanize.new 
 agent.user_agent_alias = 'Linux Mozilla'
 page = agent.get('http://localhost:3000/pictures/new')
-puts "page --> #{page.inspect}"
+#puts "page --> #{page.inspect}"
+form = page.forms.first
+fu = form.file_uploads[0]
+File.open('/home/kenb/Desktop/IMG_0862.JPG', 'r') do |f|
+  fu.file_data = f.read
+  fu.mime_type = 'image/jpg'
+end
+puts "modified form --> #{form.inspect}"
+#page = agent.submit(form)
+#puts "form --> #{form.inspect}"
+#puts "form.name -->
+
