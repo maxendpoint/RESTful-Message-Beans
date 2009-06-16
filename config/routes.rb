100dd616da8e1327922b66c4e71cc17f75995d7c
Ken Burgett <keburgett@gmail.com>
2009-06-16 11:40:23 -0700

    Adde some css magic to pretty up the listener pages

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ ActionController::Routing::Routes.draw do |map|
     #subscriber.resources :messages
   #end
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "listeners/index"
 
   # See how all your routes lay out with "rake routes"
 
