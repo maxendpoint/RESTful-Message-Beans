e924d94378b4a502765fa1b19613c0ce2514528e
Ken Burgett <keburgett@gmail.com>
2009-08-01 11:53:22 -0700

    added generate_heads view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def generate_heads(array)
+    reply = "<thead>\n<tr>\n"
+    array.each do |s|
+      reply += "<th>#{s}</th>\n"
+    end
+    reply += "</tr>\n</thead>\n"
+    reply
+  end
 end
